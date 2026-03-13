db954b35a56fc506b7026133b2eede38cb45d936
sanjidasefa <sanjidasefa9@gmail.com>
2026-03-13 19:59:54 +0600

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })
